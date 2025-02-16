0adea06fb58a611c945b1ad1acaa963ebedcd3d8
Samyak Gupta <165469687+samyakzer0@users.noreply.github.com>
2025-02-16 23:13:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <div className="app-container">
         <nav className="nav-bar glass-effect">
           <Link to="/" className="nav-logo">
+    <img src = "/favicon.png" alt= "Acute Logo" className= "logo-icon" />
             <span className="logo-text">Acute</span>
           </Link>
           <div className="nav-links">
