6575a8b7755e91032297b0033951a31446656d07
Samyak Gupta <165469687+samyakzer0@users.noreply.github.com>
2025-03-04 08:29:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Spline from '@splinetool/react-spline';
 import { motion } from 'framer-motion';
 import { ethers, BrowserProvider } from "ethers";
